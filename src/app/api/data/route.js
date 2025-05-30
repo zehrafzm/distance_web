191d6886aab3f3f8614f5eedf83782f3cb3cbbfe
zehrafzm <105994419+zehrafzm@users.noreply.github.com>
2025-05-30 23:21:12 +0300

    Update route.js
    6 distance changes

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -5,7 +5,10 @@ export async function POST(request) {
   latestData = {
     distance1: body.distance1,
     distance2: body.distance2,
-    distance3: body.distance3
+    distance3: body.distance3,
+    distance4: body.distance4,
+    distance5: body.distance5,
+    distance6: body.distance6
   };
   return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
